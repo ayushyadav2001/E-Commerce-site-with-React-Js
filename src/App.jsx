4a07a51133dd5ad93b7fbbfad6b13df0591d1e17
Ayush Yadav <65528804+ayushyadav2001@users.noreply.github.com>
2021-10-04 09:15:06 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+
+const App = () => {
+  return <Home />;
+};
+
+export default App;
